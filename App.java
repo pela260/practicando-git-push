e207fe35667fd1334db9ed228fd3a00b847fd368
Pelayo <pela260@gmail.com>
2022-12-20 13:22:03 +0100

    ultimos cambios

--- App.java
+++ App.java
@@ -1,9 +1,12 @@
 public class App {
   
   public static void main(String[] args) {
+    System.out.println("Pelayo Santos");
     System.out.println("Contenido 2a");
     System.out.println("Contenido 3");
     System.out.println("Contenido 2b");
+    System.out.println("20|12|22");
+    System.out.println("Leon");
   }
   
 }
